6be16e112e39c2ff99729f7b44929f4d873ad443
Only-San <duysannguyen113@gmail.com>
2025-12-05 20:37:08 +0700

    Doi port thanh 3001

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+        port: 3001,  // 👈 đổi port tại đây
+
     proxy: {
+      
       "/api": "http://localhost:3000",      // proxy API
       "/images": "http://localhost:3000"    // proxy ảnh
     }
